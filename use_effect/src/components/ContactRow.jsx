bda1d01eded4d769a31a3959841ae41d719c11a3
Lisa Fujita <lisafujita90@gmail.com>
2024-10-29 11:40:16 -0400

    Setting up the table

---
+++ use_effect/src/components/ContactRow.jsx
@@ -0,0 +1,13 @@
+// import ContactList from './ContactList';
+
+const ContactRow = ({ contact }) => {
+  return (
+    <tr>
+      <td>{contact.name}</td>
+      <td>{contact.email}</td>
+      <td>{contact.phone}</td>
+    </tr>
+  );
+};
+
+export default ContactRow;
